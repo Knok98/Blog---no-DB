2ee0de9d27b13550b0a439c1bab1605dfa715e9b
knok98 <petr.masa30@gmail.com>
2024-04-25 14:48:13 +0200

    delete method added

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"))
 
+let posts=[];
+let add=false;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/",(req,res)=>{
-    res.render("index.ejs")
+    res.render("index.ejs",{posts: posts})
 })
+app.get("/create",(req,res)=>{
+    res.render("index.ejs",{add: true,posts: posts})
+})
+app.post("/submit",(req,res)=>{
+    createPost(req,res);
+    res.render("index.ejs",{posts: posts,add: false})
+})
+app.post("/delete",(req,res)=>{
+    deletePost(req,res);
+    res.render("index.ejs",{posts: posts,add: false})
+})
+
 
 
 
@@ -20,3 +35,13 @@ app.get("/",(req,res)=>{
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
+
+
+function createPost(req,res){
+    const post={name: req.body["name"],article: req.body["article"],timeStamp: new Date()}
+    posts.push(post);
+};
+function deletePost(req,res){
+    console.log(req.body["id"])
+    posts.splice(req.body["id"],1)
+};
